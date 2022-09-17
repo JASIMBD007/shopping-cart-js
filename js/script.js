aad0646a33d8242f1937d75b8903445d22b49f28
Jasim Uddin <jasim.ice.iu@gmail.com>
2022-09-17 14:32:59 +0200

    subtotal,tax and total part js added

--- js/script.js
+++ js/script.js
@@ -12,17 +12,38 @@ function updateProductNumber (product, price, isIncreasing) {
     //update  total
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+
+    // calculate total 
+    calculateTotal();
 };
+
+function getInputValue (product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal () {
+
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const taxAmount = subTotal / 10;
+    const totalAmount = subTotal + taxAmount;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = taxAmount;
+    document.getElementById('total-amount').innerText = totalAmount;
+
+
+}
 
 // phone increase decrease event handler
 
 document.getElementById('phone-plus').addEventListener('click', function () {
-    console.log('phone plus clicked');
     updateProductNumber('phone', 1219, true);
 })
 
 document.getElementById('phone-minus').addEventListener('click', function () {
-    console.log('minus clicked');
     updateProductNumber('phone', 1219, false);
 })
 // case increase decrease handler
